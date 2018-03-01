7d074f135ea6f04c0dd320b5b72f421ac8fc7361
Babu <atom.ananthababu@gmail.com>
2018-03-01 16:13:36 +0530

    completed followers, following and repo list .

--- app/src/main/java/com/ananth/rxandroidwithretrofit/view/profile/ProfileViewModel.java
+++ app/src/main/java/com/ananth/rxandroidwithretrofit/view/profile/ProfileViewModel.java
@@ -16,19 +16,14 @@ import javax.inject.Inject;
 public class ProfileViewModel extends ViewModel {
 
     private final LiveData<Resource<ProfileEntity>> profileData;
-//    private final LiveData<Resource<List<FollowersEntity>>> follResourceLiveData;
 
     @Inject
     ProfileViewModel(GitRepository gitRepository) {
         profileData = gitRepository.loadProfileData();
-//        follResourceLiveData = gitRepository.loadFollowers();
     }
 
     LiveData<Resource<ProfileEntity>> getProfileData() {
         return profileData;
     }
 
-//    LiveData<Resource<List<FollowersEntity>>> getFollResourceLiveData() {
-//        return follResourceLiveData;
-//    }
 }
